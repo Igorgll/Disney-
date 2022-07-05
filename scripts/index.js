134352643ada5de0755ab3e58a9101a546850204
Igor Lima <igorlimagn@gmail.com>
2022-07-05 14:16:52 -0300

    @igorgll [Task: Performance tweaks]

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 const API_KEY = '03c4e3dc470296959d6bf68804146538'
 const API_LANGUAGE = 'pt-br'
+const BASE_URL_IMAGE = {
+    original:'https://image.tmdb.org/t/p/original',
+    small:'https://image.tmdb.org/t/p/w500'
+}
 
 const LIST_MOVIES = ['tt12801262', 'tt2948372', 'tt7146812', 'tt3521164', 'tt2948356', 'tt2096673', 'tt2380307', 'tt1049413', 'tt2953050', 'tt8097030', 'tt4823776']
 
@@ -10,10 +14,10 @@ function getUrlMovie(movieId) {
 function setFeaturedMovie(movieId) {
     fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
     console.log(data)
-    const app = document.getElementById('app')
-    const title = document.querySelector('.movie h1')
-    const description = document.querySelector('.movie p')
-    const info = document.querySelector('.movie span')
+    const appImage = document.querySelector('.app__image img')
+    const title = document.querySelector('.featured__movie h1')
+    const description = document.querySelector('.featured__movie p')
+    const info = document.querySelector('.featured__movie span')
     const rating = document.querySelector('.rating strong')
 
     const yearRelease = data.release_date.split('-')[0]
@@ -23,8 +27,8 @@ function setFeaturedMovie(movieId) {
     rating.innerHTML = data.vote_average
     info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie'
 
-    const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
-    app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
+    const image = BASE_URL_IMAGE.original.concat(data.backdrop_path)
+    appImage.setAttribute('src', image)
 })
 }
 
@@ -34,22 +38,37 @@ function createButtonMovie(movieId) {
     const button = document.createElement('button')
     button.setAttribute('onClick', `setFeaturedMovie('${movieId}')`)
     button.innerHTML = `<img src="/assets/icon-play-button.png" alt="Icon play button" />`
+    
     return button
+}
+
+function createImageMovie(movieImage, movieTitle) {
+    const divImageMovie = document.createElement('div')
+    divImageMovie.classList.add('movie__image')
+    const image = document.createElement('img')
+
+    image.setAttribute('src', movieImage)
+    image.setAttribute('alt', `Imagem do filme ${movieTitle}`)
+    image.setAttribute('loading', 'lazy')
+
+    divImageMovie.appendChild(image)
+
+    return divImageMovie
 }
 
 function createMovie(movieId) {
     console.log('createMovie id', movieId)
     fetch(getUrlMovie(movieId)).then(response => response.json()).then(data => {
         const movie = document.createElement('li')
+        movie.classList.add('movie')
         const genre = `<span>${data.genres[0].name}</span>`
         const title = `<strong>${data.title}</strong>`
-        const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
+        const image = BASE_URL_IMAGE.small.concat(data.backdrop_path)
 
         movie.innerHTML = genre + title
         movie.appendChild(createButtonMovie(movieId))
+        movie.appendChild(createImageMovie(image, data.title))
 
-        movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`
-        
         moviesList.appendChild(movie)
     })
 }
